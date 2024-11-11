3d7da07209ade2e96e70a8c5c8c87400fa0e9b1c
closecodex <closecodex@gmail.com>
2024-11-11 14:40:43 +0000

    edited project

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,5 +1,6 @@
 class PageLoadError(Exception):
     """Вызывается, когда выходит ошибка при загрузке страницы."""
 
+
 class ParserFindTagException(Exception):
     """Вызывается, когда парсер не может найти тег."""
